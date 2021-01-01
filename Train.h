99edcc8b02dea79a5761c7f648de9ed6e2285ced
NonnoPinto <giovanni.zago.3@studenti.unipd.it>
2021-01-01 14:55:08 +0100

    First Try

---
+++ Train.h
@@ -0,0 +1,17 @@
+/* author: Giovanni Zago - 1226024
+ * Questo header fa da classe principale di "Treni"
+ * Oltre al costruttore, sono presenti funzioni che regolano la distanza tra due treni
+ * e che tengono una lista ordinata dei treni in movimento*/
+ 
+class Train{
+	
+public:
+	//Costruttore della classe Train
+	//Ogni oggetto della classe ha le stesse variabili
+	//Definite in base al tipo di treno
+	Train();
+
+private:
+	
+
+};
