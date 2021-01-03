4208d35da5127ba454f2909b6cbf290ebc793f06
NonnoPinto <giovanni.zago.3@studenti.unipd.it>
2021-01-03 13:02:36 +0100

    Header virtuale

--- Train.h
+++ Train.h
@@ -14,44 +14,59 @@ public:
 	//Costruttore della classe Train
 	//Ogni oggetto della classe ha le stesse variabili
 	//Definite in base al tipo di treno
-	Train(): train_type(4)									//costruttore di default
+	Train(): nTrain(0), train_type(0), vMax(0), from (0)		//costruttore di default
 		{throw std::invalid_argument("Input needed!");};
 
 	//Il costruttore ha come argomenti
 	//tipo del treno
 	//stazione di partenza (in km) - 0 se la prima, altriemnti la lunghezza della linea
 	//gli orari di arrivo previsti per ogni stazione
-	Train(int type_, int from_, std::vector<int> &timeTable):		//costruttore - inizializza
-		train_type(type_), vCrociera(0), pos(from_), 				//tutte le varibiali membro
-		status(0), delay(0), time(timeTable){					  	//che non dipendono dal tipo
-		
-		//stazione di partenza
-		if(from_ == 0)
-			from = 0;
-		else
-			from = 1;
-		
-		//velocità massima
-		switch(type_){									//inizializza la velocità
-			case 1: vMax = 160; break;					//massima, in base al tipo
-			case 2: vMax = 240; break;					//di treno
-			case 3: vMax = 300; break;
-			default: throw std::invalid_argument("Invalid train type");
-		}
-	};
+	Train(	int n, int type_, int v,							//costruttore - inizializza
+			int from_, std::vector<int> &timeTable):			//tutte le varibiali membro
+				nTrain(n), train_type(type_), vMax(v), from(from_),
+				status(0), time(timeTable), vCrociera(0), delay(0), pos(0){
+	//DA RISOLVERE LA QUESTIONE DELLA POSIZIONE DI CHI PARTE DALLA FINE
+	if (from == 1){
+		vMax = -vMax;		//se il treno parte dall'ultima stazione, inizializzo una
+		pos = 200;}			//velcità negativa e con posizione == distanza massima
+							//dalla prima fermata
+	
+	dist_max = 10 + v/60;}; //la distanza massima è la somma dei 10 km di sicurezza
+							//più i chilometri che un treno (alla sua velocità massima)
+							//può percorrere in un minuto (unità di tempo minima)
 	
 	//calcola la posizione del treno
-	void update_pos(int time_) {pos += vCrociera/60;};
+	virtual void update_pos(const int time_) = 0;
 	
 	//controlla la distanza tra due treni (T2 è più avanti di T1)
 	//e se la distanza è < DIST_MAX, rallenta quello accodato
-	void distance(Train &T1, Train &T2) {if(T2.pos - T1.pos) T1.vCrociera = T2.vCrociera;};
+	virtual void distance(Train &T1, const Train &T2) = 0;
 	
-	//helper function per il sort
-	static bool myDist (Train &T1, Train &T2) {return (T1.pos < T2.pos);};
+	virtual void update_speed (int v) = 0;
 	
-	//merge sort
-	//mette in ordine i treni in marcia attualmente non fermi
+	
+	//***funzioni membro di Train***
+	//tiene una lista dei treni in moto 
+	void running (std::vector<Train>& v) {
+		for (int i = 0; i < v.size(); i++)
+			if(v[i].status == 1)
+				onRails_.push_back(v[i]);};
+	
+	//elimina i treni dopo che sono arrivati al capolinea
+	void arrived (Train &T) {
+		int i = -1;
+		do i++;
+		while(running_[i] == T);
+		running_.erase(running_.begin()+i);};
+	
+	//***helper functions***
+	//my_sort
+	static bool myDist (Train &T1, Train &T2)
+		{if (T1.from == 0) return (T1.pos < T2.pos);
+		 else return (T2.pos < T1.pos);};
+	
+	//STL sort
+	//mette in ordine i treni non fermi in base alla loro posizione lungo la ferrovia
 	void onRailsSort (std::vector<Train>& v){
 		std::sort (v.begin(), v.end(), myDist);
 	};
@@ -69,42 +84,37 @@ public:
 			return true;
 		else
 			return false;
-	}
+	};
 	
-	//tiene una lista dei treni in moto 
-	void running (std::vector<Train>& v) {
-		for (int i = 0; i < v.size(); i++)
-			if(v[i].status == 1)
-				onRails_.push_back(v[i]);};
 	
-	//elimina i treni dopo che sono arrivati al capolinea
-	void arrived (Train &T) {
-		int i = -1;
-		do i++;
-		while(running_[i] == T);
-		running_.erase(running_.begin()+i);};
-		
-	void update_speed (int v) {vCrociera = v;};
-
-protected:
-	//distanza massima tra due treni
-	const int DIST_MAX = 10; 					//DA CALCOLARE, NON LA RICORDO
-	
-	//velocità massima del tipo di treno
-	int vMax;
-	
-	//velocità di crociera
-	int vCrociera;
+	//***variabili membro pubbliche***
+	//(sono definite pubbliche tutte le variabili const
+	//per facilitare la comunicazioni tra classi
+	//conservando la protezione dei dati)
+	//numero del treno
+	const int nTrain;
 	
 	//tipo di treno: 1 == R, 2 == AV, 3 == AVS
 	const int train_type;
 	
 	//stazione di partenza: 0 == la prima, 1 == l'ultima
-	int from;
+	const int from;
+	
+protected:
+	//sono definite come protected tutte le varibili necessarie alle classi derivate
+	
+	//velocità massima del tipo di treno
+	int vMax;
+	
+	//distanza massima tra due treni
+	int dist_max;
+	
+	//velocità di crociera
+	int vCrociera;
 	
 	//vettore con gli orari di arrivo previsti in ogni stazione
 	//l'orario è espresso in minuti dopo la partenza
-	std::vector<int> time;
+	const std::vector<int> time;
 	
 	//varibiale di controllo sullo stato del treno
 	//0 == alla stazione di partenza, 1 == in marcia
@@ -117,11 +127,11 @@ protected:
 	//ritardo accumulato
 	int delay;
 	
+private:
+	//sono definiti private due vettori della sola classe Train
 	//treni non fermi
 	static std::vector<Train> onRails_;
 
-private:
 	//treni in marcia
 	static std::vector<Train> running_;
-
 };
