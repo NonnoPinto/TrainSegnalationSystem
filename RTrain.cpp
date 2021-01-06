00fa26a1acce2bf4723859661fa1edec65606ef7
NonnoPinto <giovanni.zago.3@studenti.unipd.it>
2021-01-06 13:41:36 +0100

    Da rendere presentabili

--- RTrain.cpp
+++ RTrain.cpp
@@ -1,27 +1,26 @@
 /* author: Giovanni Zago - 1226024
  * Classe derivata "Treni Regionali" */
 
-#pragma once
 #include "RTrain.h"
-template <typename L>
+
 
 //calcola la posizione del treno
-const int RTrain<L>::update_pos(const int time_){	//se il treno è in marcia,
-	if(this.get_status() == 1)						//aggiungo i km percorsi in un minuto
+double RTrain::update_pos(const int time_){	//se il treno è in marcia,
+	if(get_status() == 1)						//aggiungo i km percorsi in un minuto
 		pos += vCrociera/60;
 	return pos;
 }
 
-template <typename L>
+
 //calcola il ritardo
-int RTrain<L>::myDelay(const int clock_){
+int RTrain::myDelay(const int clock_){
 	station_++;
 	delay = time[station_] - clock_;
 	if (delay > 0)
-		std::cout << "Train n. " << this.nTrain << " arrived ad station n. " << station_
+		std::cout << "Train n. " << nTrain << " arrived ad station n. " << station_
 				<< " with a " << delay << " minutes delay";
 	else
-		std::cout << "Train n. " << this.nTrain << " arrived ad station n. " << station_
+		std::cout << "Train n. " << nTrain << " arrived ad station n. " << station_
 				<< " on time";
 	return delay;
 }
--- RTrain.h
+++ RTrain.h
@@ -1,32 +1,34 @@
 /* author: Giovanni Zago - 1226024
  * Header della classe derivata "Treni Regionali" */
 
-#pragma once
-#include "Train.h"
-template <typename L>
+#ifndef RTrain_h
+#define RTrain_h
 
-class RTrain : public Train<L>{
+#include "Train.h"
+
+class RTrain : public Train{
 
 public:
 	//Costruttore di default
-	RTrain() : Train<L>(), vMax(0), dist_max(0), from(0){};
+	RTrain() : Train(), vMax(0), dist_max(0), from(0){};
 	
 	//costruttore di RTrain, chiama il costruttore della classe base Train
-	RTrain(int n, int from_, std::vector<int> &timeTable): Train<L>(n),
-		from(from_), time(timeTable),
+	RTrain(int n, int from_, std::vector<int> &timeTable, std::vector<std::string> &line):
+		Train(n), from(from_),
 		vMax(160*(from_*(-1))),//se il treno parte dall'ultima stazione
 												//la velocità si considera negativa
-		dist_max((160/60))	//la distanza massima è la correzione da fare ai valori di default:
+		dist_max(10 + (160/60))	//la distanza massima è la correzione da fare ai valori di default:
 							//i chilometri che un regionale
 							//(alla sua velocità massima)
 							//può percorrere in un minuto (unità di tempo minima)
-	{if(from_==1) pos = 200;}; //DA RISOLVERE, CAZZO
+	{	set_timeTable(timeTable);
+		if(from_==1) pos = std::stoi(line[line.size()-1]);}; 
 	
-	//calcola la posizione del treno
-	const int update_pos(const int time_) override;
+	/*//calcola la posizione del treno
+	double update_pos(const int time_) override;
 	
 	//calcola il ritardo
-	int myDelay(const int clock_) override;
+	int myDelay(const int clock_) override;*/
 	
 	//tipo di treno
 	const int train_type = 1;
@@ -41,15 +43,12 @@ public:
 	const int from;
 	
 protected:
-	//aggiornamento della velocità (frenata, accelerata, stop...)
+	/*//aggiornamento della velocità (frenata, accelerata, stop...)
 	void set_speed (const int v) override {vCrociera=v;};
 	
 	//velocità di crociera
 	int vCrociera = 0;
 	
-	//vettore con gli orari di arrivo previsti in ogni stazione
-	//l'orario è espresso in minuti dopo la partenza
-	const std::vector<int> time;
 	
 	//posizione attuale del treno lungo la linea ferroviaria
 	double pos = 0;
@@ -58,5 +57,11 @@ protected:
 	int delay = 0;
 	
 	//stazioni raggiunte
-	int station_ = 0;
+	int station_ = 0;*/
+	//vettore con gli orari di arrivo previsti in ogni stazione
+	//l'orario è espresso in minuti dopo la partenza
+	//const std::vector<int> time;
+	
 };
+
+#endif
